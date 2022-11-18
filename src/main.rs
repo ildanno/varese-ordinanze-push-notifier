b1b3c2954a63c6f9b8e9128fa4babdaedd90a147
Daniele Contini <dannowonder@gmail.com>
2022-11-19 00:43:52 +0200

    notify

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+use std::env;
 use reqwest::{Client, Response, Error};
 use scraper::{Html, Selector};
+use teloxide_core::prelude::*;
+use teloxide_core::types::ForwardedFrom::Chat;
+use teloxide_core::types::ParseMode;
+use teloxide_core::types::Recipient::Id;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let chat_id = env::var("CHAT_ID").unwrap().parse::<u64>().unwrap();
+
     // https://www.comune.varese.it/c012133/zf/index.php/servizi-aggiuntivi/index/index/idtesto/565
     let res = Client::default().get("https://www.comune.varese.it/c012133/zf/index.php/servizi-aggiuntivi/index/index/idtesto/565");
     let body = res.send().await?;
@@ -10,11 +17,13 @@ async fn main() -> Result<(), Error> {
     let fragment = Html::parse_document(body.text().await?.as_str());
     let selector = Selector::parse("#main > div > div.Grid.Grid--withGutter > div > ul > li > a").unwrap();
 
-    for element in fragment.select(&selector) {
-        let text: String = element.text().collect::<Vec<_>>().join(" ");
-        let url = element.value().attr("href").unwrap_or_default();
-        println!("{:?}", text);
-        println!("{:?}", url);
+    let bot = Bot::from_env().parse_mode(ParseMode::Html);
+
+    for x in fragment.select(&selector) {
+        let text = x.text().collect::<Vec<_>>().join(" ");
+        let url = x.value().attr("href").unwrap_or_default();
+        let body = format!("<b>Nuova Ordinanza Comune di Varese</b>\n\n<a href=\"{}\">{}</a>", url, text);
+        bot.send_message(UserId(chat_id), body).await.unwrap();
     }
 
     Ok(())
